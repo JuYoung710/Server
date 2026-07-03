8be67e68b9a9dc293bd9d8d21f967aefb3643024
조주영 <wndudquf@naver.com>
2026-07-03 10:57:18 +0900

    포스트 쓰기 API 완료

--- app.mjs
+++ app.mjs
@@ -8,7 +8,7 @@ const app = express()
 
 app.use(express.json())
 app.use("/auth", authRouter )
-app.use("/posts", postsRouter)
+app.use("/post", postsRouter)
 
 app.use((req, res) => {
     res.sendStatus(404)
--- router/posts.mjs
+++ router/posts.mjs
@@ -1,12 +1,17 @@
 import express from "express"
+import { isAuth } from "../middleware/auth.mjs"
+import * as postController from "../controller/posts.mjs"
 
 const router = express.Router()
 
 // 전체 포스트 가져오기
 
+
 // 글번호에 대한 포스트 가져오기
 
 // 포스트 작성
+// http://127.0.0.1:8080/post
+router.post("/", isAuth, postController.createPost)
 
 // 포스트 수정
 
